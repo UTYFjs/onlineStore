806969a467080cb7698e32f5e9e13fd1b905af60
UTYFjs <utyf-90@mail.ru>
2023-02-24 13:51:02 +0400

    fix cardProduct height hover effect

--- src/components/CardProduct/CardProduct.tsx
+++ src/components/CardProduct/CardProduct.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { IDataProduct } from '../../data/dataProducts';
 import { useFavoriteStore } from '../../store/zustandStore';
+import Button from '../Button/Button';
 import ImgLazy from '../ImgLazy/ImgLazy';
 import styles from './CardProduct.module.scss';
 interface ICardProductProps {
@@ -27,28 +28,36 @@ function CardProduct({ data }: ICardProductProps) {
 
   return (
     <div className={styles.card}>
+      <div className={styles.fake}>
+        <div className={styles['fake-content']}></div>
+        <div className={styles['img-wrapper']}></div>
+        <h4 className={styles['card-title']}>{title}</h4>
+        <p className={styles['card-price']}>{price + ' gel'}</p>
+
+        <div className={styles['fake-nav']}>
+          <div className={styles.embossing}>
+            <label htmlFor={id}>Embossing (price: +10)</label>
+            <input
+              type="checkbox"
+              id={id}
+              className={styles['embossing-checkbox']}
+              onInput={(e) => setEmbossing(e.currentTarget.checked)}
+            />
+          </div>
+          <Button content={'Add to cart'} />
+          <div
+            className={styles['heart-1']}
+            style={getStyleFavorite()}
+            onClick={handleToggleFavorite}
+          />
+        </div>
+      </div>
+
       <div className={styles['img-wrapper']}>
         <ImgLazy src={thumbnail.primary} alt={title} />
       </div>
       <h4 className={styles['card-title']}>{title}</h4>
-      <div className={styles.embossing}>
-        <label htmlFor={id}>Embossing (price: +10)</label>
-        <input
-          type="checkbox"
-          id={id}
-          className={styles['embossing-checkbox']}
-          onInput={(e) => setEmbossing(e.currentTarget.checked)}
-        />
-      </div>
-      <p className={styles['card-price']}>{price}</p>
-
-      <button className={styles['card-add-cart']}>add Cart</button>
-
-      <div
-        className={styles['heart-1']}
-        style={getStyleFavorite()}
-        onClick={handleToggleFavorite}
-      />
+      <p className={styles['card-price']}>{price + ' gel'}</p>
     </div>
   );
 }
